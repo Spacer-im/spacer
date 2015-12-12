5ae48a53fe54dc1a3017251380c2edb23c61d9c9
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-12-12 19:35:50 +0300

    update showcase and fix for mobile

--- client/views/includes/showcase.js
+++ client/views/includes/showcase.js
@@ -1,6 +1,6 @@
 Template.showcase.helpers({
    pagePhrase: function () {
-       return Session.get("pagePhrase") || "Participate in space exploration and launch your space career";
+       return Session.get("pagePhrase") || "Your space career toolkit";
    },
     pageTitle: function () {
         return Session.get("pageTitle") || "Spacer";
