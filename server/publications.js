1e6e3ab81ca24374a3792b26df2df62b3ed00be2
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-12-15 17:03:35 +0300

    add tags in news

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,9 @@ Meteor.publish('news_titles', function () {
 Meteor.publish("news_article", function(newsId) {
     return News.find({_id: newsId});
 });
+
+Meteor.publish("tags", function() {
+    return Tags.find({});
+});
+
+
