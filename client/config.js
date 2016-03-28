bac0adf8f3c89a3edabf0d248c7852ec91c504c0
Valentin Bryukhanov <bvv.mag@gmail.com>
2016-03-28 14:44:23 +0300

    fix(client): header footer order

--- client/config.js
+++ client/config.js
@@ -1,5 +1,14 @@
 Accounts.ui.config({
     forceEmailLowercase: true,
     forceUsernameLowercase: true,
-    passwordSignupFields: "USERNAME_AND_EMAIL"
+    passwordSignupFields: "USERNAME_AND_EMAIL",
+    extraSignupFields: [
+        {
+            fieldName: 'subscribeDigest',
+            fieldLabel: 'Keep me updated on the latest space news and career opportunities',
+            inputType: 'checkbox',
+            visible: true,
+            saveToProfile: true
+        }
+    ]
 });
--- client/views/includes/accounts.js
+++ client/views/includes/accounts.js
@@ -1,36 +1,40 @@
-Template.login.rendered = function()
-{
+Template.login.rendered = function () {
     Accounts._loginButtonsSession.set('dropdownVisible', true);
 };
 
-Template.registerHelper("isAdmin", function() {
+Template.login.helpers({
+    isSignUp: () =>Accounts._loginButtonsSession.get("inSignupFlow")
+});
+
+
+Template.registerHelper("isAdmin", function () {
     let loggedUser = Meteor.user();
     return loggedUser && Roles.userIsInRole(loggedUser, ['admin']);
 });
 
 Template._loginButtonsLoggedInDropdown.events({
-    'click #login-buttons-profile': function(event) {
+    'click #login-buttons-profile': function (event) {
         Router.go('/profile');
     },
-    'click #login-buttons-admin': function(event) {
+    'click #login-buttons-admin': function (event) {
         Router.go('/admin');
     },
-    'click #login-buttons-add-news': function(event) {
+    'click #login-buttons-add-news': function (event) {
         Router.go('/news-add');
     },
-    'click #login-buttons-add-company': function(event) {
+    'click #login-buttons-add-company': function (event) {
         Router.go('/company-add');
     },
-    'click #login-buttons-add-job': function(event) {
+    'click #login-buttons-add-job': function (event) {
         Router.go('/job-add');
     },
-    'click #login-buttons-add-project': function(event) {
+    'click #login-buttons-add-project': function (event) {
         Router.go('/project-add');
     }
 });
 
 Template._loginButtonsLoggedInDropdown.helpers({
-    user_profile_picture: function() {
+    user_profile_picture: function () {
         var user = Meteor.user();
         if (user && user.avatarUrl()) {
             return user.avatarUrl();
@@ -38,3 +42,18 @@ Template._loginButtonsLoggedInDropdown.helpers({
         return "";
     }
 });
+
+Template._loginButtonsFormField.onRendered(function () {
+    const checkbox = document.getElementById('login-subscribeDigest');
+    if (checkbox) {
+        checkbox.checked = true;
+    }
+});
+
+accountsUIBootstrap3.setCustomSignupOptions = function() {
+    const checkbox = document.getElementById('login-subscribeDigest');
+    console.log(checkbox.checked);
+    return {
+        subscribeDigest:
+    }
+};
