08eb83cc9100313e93b837515aa8ffdd652b6150
Valentin Bryukhanov <bvv.mag@gmail.com>
2016-04-20 11:10:40 +0300

    fix(all): fix createUser bug

--- server/accounts.js
+++ server/accounts.js
@@ -1,10 +1,7 @@
-//Accounts.onCreateUser(function (user, options) {
-//    console.log(user);
-//    console.log(options);
-//    if (options.profile) {
-//        user.profile = options.profile;
-//    }
-//    user.emails = options.emails || [];
-//    user.createdAt = new Date();
-//    return user;
-//});
+Accounts.onCreateUser(function (options, user) {
+    if (options.profile) {
+        user.profile = options.profile;
+    }
+    user.createdAt = new Date();
+    return user;
+});
